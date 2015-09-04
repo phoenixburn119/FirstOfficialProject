8a2dfa626921f04568106b9675f57175d2ad79a8
Adam Kinzer <adamkinzer915@gmail.com>
2015-09-04 13:26:18 -0600

    Added code to the repo.
    My “First” code push ever!

---
+++ src/input/controller/InputController.java
@@ -0,0 +1,39 @@
+package input.controller;
+
+import java.util.Scanner;
+
+public class InputController
+{
+	//Declaration section
+	//All class data members are declared private
+	
+	private Scanner myInput;
+	
+	//Constructors go here
+	
+	public InputController()
+	{
+		myInput = new Scanner(System.in);
+	}
+	
+	public void start()
+	{
+		questions();
+	}
+	
+	private void questions()
+	{
+		System.out.println("Hey, what is your name");
+		String input;
+		input = myInput.next();
+		myInput.nextLine();
+		System.out.println("Your name is " + input + ", that's cool cool");
+		System.out.println("oops did I miss your full name? Please type it again.");
+		String fullName = myInput.nextLine();
+		System.out.println("Your full name is " + fullName);
+		System.out.println("Your age if you do not mind my good sir?");
+		int myAge = myInput.nextInt();
+		System.out.println("Wow " + input + " you are " + myAge + " years old!");
+	}
+	
+}
